fee852b32fae6c21d0d749a1e610dd46b0362775
0x4FF <112463082+0x4FF@users.noreply.github.com>
2022-10-04 13:19:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
-import pefile
-from capstone import *
+# @ waves
+import pefile,sys,os;from capstone import *;
 
-def PE_FORMAT_DISPLAY():
-    file = input(".exe file name: ")
-    exe = pefile.PE(file)
-    with open('pe_format_log.txt', 'w+') as file:
-        file.write(str(exe))
 
-def EXE_DISASSEMBLE():
-    file = input(".exe file name: ")
-    exe = pefile.PE(file)
-    e_p = exe.OPTIONAL_HEADER.AddressOfEntryPoint
-    data = exe.get_memory_mapped_image()[e_p:]
-    cs = Cs(CS_ARCH_X86, CS_MODE_32)
-    rdbin = cs.disasm(data, 0x1000)
-    with open('disam_code.txt', 'w+') as file:
-        for i in rdbin:
-            file.write("0x%x:\t%s\t%s\n" %(i.address, i.mnemonic, i.op_str))
+if os.name == 'nt': pip = "pip";clear = "cls"
+elif os.name == 'posix': pip = "pip3";clear = "clear"
+
+
+try:
+    os.system(f"{pip} install capstone")
+    os.system(f"{pip} install pefile")
+except:
+    pass
+
+
+class EXE_DISASM:
+    def __init__(self, file):
+            self.file = file
+
+    def asam_dump(self,file):
+            file_loaded = pefile.PE(file)
+            file_entry_point = file_loaded.OPTIONAL_HEADER.AddressOfEntryPoint
+            data = file_loaded.get_memory_mapped_image()[file_entry_point:]
+            cs = Cs(CS_ARCH_X86, CS_MODE_32)
+            rdbin = cs.disasm(data, 0x1000)
+            for i in rdbin:
+                print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+
+
+class PE_FILE_FORMAT:
+        def __init__(self, file):
+            self.file = file
+
+        def show_format(self, file):
+            exe = pefile.PE(file)
+            print(str(exe))
 
 
 def main():
-    choice = input("[1] PE Breakdown   [2] Disassemble EXE: ")
-    if choice == "1":
-        PE_FORMAT_DISPLAY()
-        main()
-    elif choice == "2":
-        EXE_DISASSEMBLE()
-        main()
+        os.system(clear)
+        print(f"""Options: -p, -d
+        \nUsage: ./{sys.argv[0]} <option> <file>""")
+
+        choice = sys.argv[1]
+        if choice == "-p":
+                FILE__ = PE_FILE_FORMAT(sys.argv[2])
+                FILE__.show_format(sys.argv[2])
+        elif choice == "-d":
+                FILE_ = EXE_DISASM(sys.argv[2])
+                FILE_.asam_dump(sys.argv[2])
 
 
-main()
+if __name__ == "__main__":
+    main()
