dfb6ebdfddb6449cd678e07d38c342a8fcb1fb1c
0x4FF <112463082+0x4FF@users.noreply.github.com>
2022-10-08 22:20:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,47 @@
 # @ waves
-import pefile,sys,os;from capstone import *;
+from os.path import exists
+from capstone import *
+import argparse
+import pefile
+import sys
+import os
 
 
-if os.name == 'nt': pip = "pip";clear = "cls"
-elif os.name == 'posix': pip = "pip3";clear = "clear"
+def asam_dump():
+    file_loaded = pefile.PE(sys.argv[2])
+    with open((f"{sys.argv[4]}"), 'x+') as file:
+        _entry_point = file_loaded.OPTIONAL_HEADER.AddressOfEntryPoint
+        data = file_loaded.get_memory_mapped_image()[_entry_point:]
+        cs = Cs(CS_ARCH_X86, CS_MODE_64)
+        rdbin = cs.disasm(data, 0x1000)
+        for instruc in rdbin:
+            file.write(f"0x{instruc.address}:\t{instruc.mnemonic}\t{instruc.op_str}\n")
+        print(f"[+] Succesfully disassembled {sys.argv[2]}")
 
-
-try:
-    os.system(f"{pip} install capstone")
-    os.system(f"{pip} install pefile")
-except:
-    pass
-
-
-class EXE_DISASM:
-    def __init__(self, file):
-            self.file = file
-
-    def asam_dump(self,file):
-            file_loaded = pefile.PE(file)
-            file_entry_point = file_loaded.OPTIONAL_HEADER.AddressOfEntryPoint
-            data = file_loaded.get_memory_mapped_image()[file_entry_point:]
-            cs = Cs(CS_ARCH_X86, CS_MODE_32)
-            rdbin = cs.disasm(data, 0x1000)
-            for i in rdbin:
-                print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
-
-
-class PE_FILE_FORMAT:
-        def __init__(self, file):
-            self.file = file
-
-        def show_format(self, file):
-            exe = pefile.PE(file)
-            print(str(exe))
+def show_format():
+    pe = pefile.PE(sys.argv[2])
+    with open(f'{sys.argv[4]}', 'x+') as file:
+            file.write(f"{str(pe)}")
+    print(f"[+] Succesfully brokedown file {sys.argv[2]}")
 
 
 def main():
-        os.system(clear)
-        print(f"""Options: -p, -d
-        \nUsage: ./{sys.argv[0]} <option> <file>""")
+    parser = argparse.ArgumentParser()
 
-        choice = sys.argv[1]
-        if choice == "-p":
-                FILE__ = PE_FILE_FORMAT(sys.argv[2])
-                FILE__.show_format(sys.argv[2])
-        elif choice == "-d":
-                FILE_ = EXE_DISASM(sys.argv[2])
-                FILE_.asam_dump(sys.argv[2])
+    parser.add_argument("-d", 
+                        help="Disassemble file and show assembly code")
+    parser.add_argument("-p", 
+                        help="Show PE file information")
+    parser.add_argument("-s",
+                        help="Enter file name to save output",
+                        action='store')
+
+    cli_args = parser.parse_args()
+
+    if cli_args.p:
+        show_format()
+    elif cli_args.d:
+        asam_dump()
 
 
 if __name__ == "__main__":
